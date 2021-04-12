6d38af5fb68d3fd87943e992adb5f34a87ac68b2
Wout02 <55230010+Wout02@users.noreply.github.com>
2021-04-12 13:11:36 +0200

    Add files via upload

---
+++ main/include/radio.h
@@ -0,0 +1,6 @@
+#define STREAM_1 "http://open.ls.qingting.fm/live/274/64k.m3u8?format=aac"
+#define STREAM_2 "http://playerservices.streamtheworld.com/api/livestream-redirect/RADIO538AAC.aac"
+
+void start_stream(char stream);
+void stop_radio();
+
